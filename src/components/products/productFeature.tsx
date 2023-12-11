886893a45171d6f1bac2b140e9216d25170604e2
Jon Ander Oribe <jonan.oribe@gmail.com>
2023-12-11 10:24:14 +0100

    Basics

--- src/components/products/productFeature.tsx
+++ src/components/products/productFeature.tsx
@@ -34,8 +34,8 @@ export default function ProductOverview({
       <div className="row mt-5">
         <div className="col-12 col-lg-6 pe-5">
           <div className="row">
-            <h4 className="mb-3">Product Description</h4>
-            <p className="mb-5">Society has put up so many boundaries, so many limitations on what’s right and wrong that it’s almost impossible to get a pure thought out. It’s like a little kid, a little boy, looking at colors, and no one told him what colors are good, before somebody tells you you shouldn’t like pink because that’s for girls, or you’d instantly become a gay two-year-old.</p>
+            <h4 className="mb-3">Certificados</h4>
+            <p className="mb-5">Nuestro personal esta acreditado y formado en la Prevencion de Riesgos Laborales disponiendo de los EPIS correspondiente a las actividades a desarrollar. Además contamos con Seguro de Responsabilidad Civil para responder ante nuestros Clientes de cualquier accidente o negligencia sobrevenida en la prestación de los servicios contratados.</p>
             <ProductFeatureDetails featuresDetails={featuresDetails} />
           </div>
         </div>

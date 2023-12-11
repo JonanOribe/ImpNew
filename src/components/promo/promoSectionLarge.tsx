ce64d124f985debeaf109c50c96a242e63241423
Jon Ander Oribe <jonan.oribe@gmail.com>
2023-12-11 09:06:14 +0100

    Basics

--- src/components/promo/promoSectionLarge.tsx
+++ src/components/promo/promoSectionLarge.tsx
@@ -38,7 +38,7 @@ export default function TestimonialsFade({
               <div className="col-lg-8 col-sm-9 text-center mx-auto">
                 <h1 className="text-white mb-4">{title}</h1>
                 <p className="lead text-white mb-sm-6 mb-4">{full_description}</p>
-                <button className="btn btn-white btn-lg">Explore New Collection</button>
+                <button className="btn btn-white btn-lg" style={{background: "#143575",color: "white"}}>Contáctenos</button>
               </div>
             </div>
           </div>
